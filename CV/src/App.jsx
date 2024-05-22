76094a641243037fcc714cc8fa3bf0668bfd964f
Darien Lin <darienlin87@gmail.com>
2024-05-22 15:50:25 -0700

    user input now appears on resume

--- CV/src/App.jsx
+++ CV/src/App.jsx
@@ -5,9 +5,54 @@ import './styles/App.css'
 
 function App() {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhoneNumber] = useState("");
+  const [schoolName, setSchoolName] = useState("");
+  const [major, setMajor] = useState("");
+  const [graduationDate, setGraduationDate] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [title, setTitle] = useState("");
+  const [responsibilites, setResonsibilites] = useState("");
+  const [dateWorked, setDateWorked] = useState("");
 
-  const handleChange = (newValue) => {
+  const handleName = (newValue) => {
     setName(newValue);
+  };
+
+  const handleEmail = (newValue) => {
+    setEmail(newValue);
+  };
+
+  const handlePhone = (newValue) => {
+    setPhoneNumber(newValue);
+  };
+
+  const handleSchool = (newValue) => {
+    setSchoolName(newValue);
+  };
+
+  const handleMajor = (newValue) => {
+    setMajor(newValue);
+  };
+
+  const handleGraduation = (newValue) => {
+    setGraduationDate(newValue);
+  };
+
+  const handleCompany = (newValue) => {
+    setCompanyName(newValue);
+  };
+
+  const handleTitle = (newValue) => {
+    setTitle(newValue);
+  };
+
+  const handleResonsibilites = (newValue) => {
+    setResonsibilites(newValue);
+  };
+
+  const handleDateWorked = (newValue) => {
+    setDateWorked(newValue);
   };
 
   return (
@@ -18,15 +63,15 @@ function App() {
           <div className='generalInformation'>
             <div className='formBlock'>
               <div className='label'>Name</div>
-              <CustomInput placeholder='anakin skywalker' onChange={handleChange}/>
+              <CustomInput placeholder='anakin skywalker' onChange={handleName}/>
             </div>
             <div className='formBlock'>
               <div className='label'>Email</div>
-              <CustomInput placeholder='notVader@gmail.com' />
+              <CustomInput placeholder='notVader@gmail.com' onChange={handleEmail}/>
             </div>
             <div className='formBlock'>
               <div className='label'>Phone number</div>
-              <CustomInput placeholder='123-456-789' />
+              <CustomInput placeholder='123-456-789' onChange={handlePhone}/>
             </div>
           </div>
         </div>
@@ -37,15 +82,15 @@ function App() {
           <div className='generalInformation'>
             <div className='formBlock'>
               <div className='label'>School Name</div>
-              <CustomInput placeholder='Cal Poly Pomona' />
+              <CustomInput placeholder='Cal Poly Pomona' onChange={handleSchool}/>
             </div>
             <div className='formBlock'>
               <div className='label'>Major</div>
-              <CustomInput placeholder='Computer Science' />
+              <CustomInput placeholder='Computer Science' onChange={handleMajor}/>
             </div>
             <div className='formBlock'>
               <div className='label'>Graduation Date</div>
-              <CustomInput placeholder='05/2026' />
+              <CustomInput placeholder='05/2026' onChange={handleGraduation}/>
             </div>
           </div>
         </div>
@@ -55,26 +100,40 @@ function App() {
           <div className='generalInformation'>
             <div className='formBlock'>
               <div className='label'>Company Name</div>
-              <CustomInput placeholder='CPP' />
+              <CustomInput placeholder='CPP' onChange={handleCompany}/>
             </div>
             <div className='formBlock'>
               <div className='label'>Position title</div>
-              <CustomInput placeholder='SWE Intern' />
+              <CustomInput placeholder='SWE Intern' onChange={handleTitle} />
             </div>
             <div className='formBlock'>
               <div className='label'>Responsibilites</div>
-              <CustomInput placeholder='Inventory Mangagement' />
+              <CustomInput placeholder='Inventory Mangagement' onChange={handleResonsibilites}/>
             </div>
             <div className='formBlock'>
               <div className='label'>Date worked</div>
-              <CustomInput placeholder='05/2026-03/2029' />
+              <CustomInput placeholder='05/2026-03/2029' onChange={handleDateWorked} />
             </div>
           </div>
         </div>
       </div>
 
       <div className='resume'>
-        {name}
+        <div className='contactInformation'>
+        <div className='name'>{name}</div>
+        <div>{phone} | {email}</div>
+        </div>
+        <div className='resumeSubtitle'>Education</div>
+        <div>{schoolName} Expected graduation: {graduationDate}</div>
+        <div>{major}</div>
+
+        <div>
+          <div className='resumeSubtitle'>Professional Experience</div>
+          <div>{companyName}</div>
+          <div>{title}</div>
+          <div>{responsibilites}</div>
+          <div>{dateWorked}</div>
+        </div>
       </div>
 
     </div>
